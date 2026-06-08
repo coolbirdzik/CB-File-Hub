b57eda419227d30f4350bbeaf9556061b6c9432f
Tan Hung <ngtanhung41@gmail.com>
2026-06-08 17:13:41 +0700

    fix: remove NDK ABI filters from build configuration

--- cb_file_manager/android/app/build.gradle.kts
+++ cb_file_manager/android/app/build.gradle.kts
@@ -52,11 +52,6 @@ android {
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
-
-        ndk {
-            abiFilters.clear()
-            abiFilters.addAll(listOf("arm64-v8a", "x86_64"))
-        }
     }
 
     packaging {
